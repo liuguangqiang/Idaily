31ac568e7c01be3b052677f16c1ac369595eb3a6
Eric <guangqiang.dev@gmail.com>
2015-07-27 11:03:47 +0800

    Fixed.

---
+++ app/src/main/java/com/liuguangqiang/idaily/app/DailyApplication.java
@@ -0,0 +1,15 @@
+package com.liuguangqiang.idaily.app;
+
+import android.app.Application;
+
+/**
+ * Created by Eric on 15/6/29.
+ */
+public class DailyApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
