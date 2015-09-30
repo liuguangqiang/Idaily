623fe5fc3cd1aa7cea8f99c505c8bd89d4b692d4
Eric <guangqiang.dev@gmail.com>
2015-09-30 21:39:57 +0800

    Print logs just only in debug model.

--- app/src/main/java/com/liuguangqiang/idaily/app/DailyApplication.java
+++ app/src/main/java/com/liuguangqiang/idaily/app/DailyApplication.java
@@ -2,6 +2,9 @@ package com.liuguangqiang.idaily.app;
 
 import android.app.Application;
 
+import com.liuguangqiang.framework.utils.Logs;
+import com.liuguangqiang.idaily.BuildConfig;
+
 /**
  * Created by Eric on 15/6/29.
  */
@@ -10,6 +13,7 @@ public class DailyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Logs.setIsDebug(BuildConfig.DEBUG);
     }
 
 }
